2cc67d11ff7cc13c51dcd8ba62fc5bee378d6e84
JplsMura13 <joao.pedro@jhe.com.br>
2026-05-14 17:45:22 -0300

    fix: dark mode toggle ghost icon bug

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,28 +1,29 @@
 export const initTheme = () => {
     const themeButton = document.getElementById('theme-button');
+    const themeIcon = themeButton.querySelector('i');
     const lightTheme = 'light-theme';
-    const iconTheme = 'bx-sun';
 
-    // Previously selected topic (if user selected)
     const selectedTheme = localStorage.getItem('selected-theme');
     const selectedIcon = localStorage.getItem('selected-icon');
-
-    // We obtain the current theme that the interface has by validating the light-theme class
     const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'light' : 'dark';
-    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+    const getCurrentIcon = () => themeIcon.classList.contains('bx-sun') ? 'bx-sun' : 'bx-moon';
 
-    // We validate if the user previously chose a topic
     if (selectedTheme) {
       document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](lightTheme);
-      themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+      if (selectedIcon === 'bx-sun') {
+          themeIcon.classList.remove('bx-moon');
+          themeIcon.classList.add('bx-sun');
+      } else {
+          themeIcon.classList.remove('bx-sun');
+          themeIcon.classList.add('bx-moon');
+      }
     }
 
-    // Activate / deactivate the theme manually with the button
     themeButton.addEventListener('click', () => {
-        // Add or remove the light / icon theme
         document.body.classList.toggle(lightTheme);
-        themeButton.classList.toggle(iconTheme);
-        // We save the theme and the current icon that the user chose
+        themeIcon.classList.toggle('bx-sun');
+        themeIcon.classList.toggle('bx-moon');
+        
         localStorage.setItem('selected-theme', getCurrentTheme());
         localStorage.setItem('selected-icon', getCurrentIcon());
     });
